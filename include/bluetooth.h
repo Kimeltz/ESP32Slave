d69d7f7b2f645754a43a17386930933b50cc2614
Kimeltz <ikmaltsani0@gmail.com>
2025-04-09 16:53:40 +0700

    Added Bluetooth

---
+++ include/bluetooth.h
@@ -0,0 +1,76 @@
+#ifndef BLUETOOTH_H
+#define BLUETOOTH_H
+#include <BluetoothSerial.h>
+class BluetoothComm {
+    private:
+      BluetoothSerial btSerial;
+      String deviceName;
+    
+    public:
+        BluetoothComm(const String& name = "ESP32-BT") : deviceName(name) {}
+
+        // Init bluetooth
+        void begin() {
+        if (!btSerial.begin(deviceName)) {
+            Serial.println("❌ Bluetooth init gagal");
+        } else {
+            Serial.printf("📡 Bluetooth aktif sebagai \"%s\"\n", deviceName.c_str());
+        }
+        }
+
+        // Kirim data (string atau angka)
+        void send(const String& data) {
+            btSerial.println(data);
+        }
+
+        void send(int data) {
+            btSerial.println(data);
+        }
+
+        // Read data
+        bool available() {
+            return btSerial.available();
+        }
+
+        String readLine() {
+        String incoming = "";
+        while (btSerial.available()) {
+                char c = btSerial.read();
+                if (c == '\n') break;
+                incoming += c;
+            }
+            return incoming;
+        }
+
+        BluetoothSerial& getSerial() {
+            return btSerial;
+        }
+};
+
+
+#endif
+
+/*
+*** Example ***
+
+#include "bluetooth.h"
+
+BluetoothComm bt("FireMonitor-ESP");
+
+void setup() {
+  Serial.begin(115200);
+  bt.begin(); // Aktifkan Bluetooth dengan nama "FireMonitor-ESP"
+}
+
+void loop() {
+  if (bt.available()) {
+    String msg = bt.readLine();
+    Serial.print("📥 BT Received: ");
+    Serial.println(msg);
+  }
+
+  bt.send("🔥 Status aman");
+  delay(2000);
+}
+
+*/
