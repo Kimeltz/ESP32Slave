b48736a9cf0e16704fe5f8e441bf85633c8cc4e3
Kimeltz <ikmaltsani0@gmail.com>
2025-04-10 23:07:38 +0700

    Turning Parser funcs into header instead

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,8 @@ uint64_t lastDataRead = 0;
 void printAddress(DeviceAddress deviceAddress);
 void init();
 void readData();
+void freeParse(char** buffer, int maxBuffer);
+char** parse(char* input, const char* delim, int maxBuffer = 5);
 
 
 void setup() {
@@ -140,32 +142,4 @@ void init()
       while (1);
     }    
   }
-}
-
-char** parse(char* input, const char* delim, int maxBuffer = 5)
-{
-  if (input == NULL || delim == NULL) return NULL;
-
-  char** buffer = (char**)calloc(maxBuffer, sizeof(char*));
-  if (!buffer) {
-    Serial.println("❌ Gagal mengalokasikan memori untuk buffer parse");
-    return NULL;
-  }
-
-  int index = 0;
-  char* token = strtok(input, delim);
-  while (token != NULL && index < maxBuffer - 1) {
-    buffer[index] = strdup(token);  // alokasikan dan salin
-    if (!buffer[index]) {
-      Serial.printf("❌ Gagal alokasi untuk token ke-%d\n", index);
-      // Bersihkan alokasi sebelumnya
-      for (int j = 0; j < index; j++) free(buffer[j]);
-      free(buffer);
-      return NULL;
-    }
-    index++;
-    token = strtok(NULL, delim);
-  }
-
-  return buffer;
 }
